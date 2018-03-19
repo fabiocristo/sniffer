97b3bf5def37934d1cd8c8984dc0b07f0ea7fdb1
Fábio Cristofoline <fabiocristofoline@gmail.com>
2018-03-19 09:22:28 -0300

    Update sniffer.py

--- sniffer.py
+++ sniffer.py
@@ -2,5 +2,19 @@ from socket import *
 
 s = socket(AF_INET, SOCK_RAW, IPPROTO_TCP)
 
+n = 1
 while 1:
-  print(s.recvfrom(65565).decode())
+    print('Number ', n)
+    data = s.recvfrom(65565)
+    packet = data[0]
+    address = data[1]
+    header = struct.unpack('!BBHHHBBHBBBBBBBB', packet[:20])
+    if header[6] == 6:
+        print("Protocolo: TCP")
+    elif header[6] == 17:
+        print("Protocolo: UDP")
+    elif header[5] == 1:
+        print("Protocolo: ICMP") 
+    print("Address: ", address)
+    print("Data: ", data)
+    n = n + 1
